5f46da13a1b9c84d4c7193e3c04687b0e7d967ba
AV <av@admindeMacBook-Pro.local>
2017-11-28 11:07:19 +0800

    feat:修改了mongodb的连接字符串

--- database/mongodb.js
+++ database/mongodb.js
@@ -2,7 +2,7 @@ let MongoClient = require('mongodb').MongoClient
 
 module.exports = {
     getDB(db, db_config) {
-        let url = `mongodb://${db_config.connection_url}/${db}`
+        let url = `mongodb://${db_config.auth?(db_config.auth.user+':'+db_config.auth.pwd+'@'):''}${db_config.connection_url}/${db}`
         return MongoClient.connect(url, db_config.options)
     }
 }
